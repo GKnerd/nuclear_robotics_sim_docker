a314ba748127f46b94144159fe120c3ecf38a9e8
Georgios Katranis <georg.katranis@gmail.com>
2026-06-15 21:49:07 +0200

    Minor change to -sh

--- docker/run_container_on_mac.sh
+++ docker/run_container_on_mac.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-#    Copyright 2026 Proximity Robotics & Automation GmbH
-
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-
-#        http://www.apache.org/licenses/LICENSE-2.0
-
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-
 # macOS (Apple Silicon) run script. Key differences from run_container.sh:
 #   * No X11 forwarding (xhost / DISPLAY / .X11-unix / .Xauthority) — macOS has
 #     no native X server. The GUI lives inside the container and is reached in a
